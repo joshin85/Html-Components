40af5b0a6e52760589c5624030c0d160372e99ff
Shinjo Melosh <joshin85@gmail.com>
2016-02-17 14:34:08 -0800

    overwindow updated layout
    Updated layout for overwindow component - added social buttons and
    tweaked responsiveness

--- overwindow/js/overwindow.js
+++ overwindow/js/overwindow.js
@@ -1,10 +1,18 @@
 $(function(){
+	
+	validate($(".window-container .form-container"));
+	
 	$("button").on("click", function(){
 		$(".window-container").addClass("active");
 	});
 	
 	$(".close").on("click", function(){
 		$(".window-container").removeClass("active");
+	});
+	
+	$(".rotational-btn-group .btn").on("click", function(){
+		$(".rotational-btn-group .btn").removeClass("active");
+		$(this).addClass("active");
 	});
 	
 });
